d02f594b055f5e742969174453bd933ce9df6029
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-09-19 14:47:36 -0300

    feat: installing and configure prismaORM

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { routes } from "./routes";
 
 const server = Fastify({ logger: true });
 
 const start = async () => {
+    await server.register(cors);
+    await server.register(routes);
+
     try {
         await server.listen({ port: 3333 })
     } catch {
         process.exit(1)
     }
 }
+
+start();
