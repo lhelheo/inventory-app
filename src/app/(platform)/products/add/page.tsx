dda2c1185629d26efec0bb2cf3af5bf7331fc874
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-10-04 15:48:48 -0300

    fix: routes again

--- src/app/(platform)/products/add/page.tsx
+++ src/app/(platform)/products/add/page.tsx
@@ -17,7 +17,7 @@ export default function CreateProduct() {
     useEffect(() => {
         async function fetchClients() {
             try {
-                const response = await axios.get(`${baseUrl}/client`);
+                const response = await axios.get(`${baseUrl}/clients`);
                 setClients(response.data);
             } catch (error) {
                 if (axios.isAxiosError(error) && error.response) {
