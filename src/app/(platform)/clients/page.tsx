a18e03f45e0a41266a5e34937211ae22814f336e
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-10-04 15:52:18 -0300

    feat: loading to get all clients

--- src/app/(platform)/clients/page.tsx
+++ src/app/(platform)/clients/page.tsx
@@ -9,14 +9,21 @@ import { useRouter } from "next/navigation";
 export default function Clients() {
   const router = useRouter();
   const [customers, setCustomers] = useState<ICustomer[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loadCustomers();
   }, []);
 
   async function loadCustomers() {
-    const response = await api.get(`${baseUrl}/clients`);
-    setCustomers(response.data);
+    try {
+      const response = await api.get(`${baseUrl}/clients`);
+      setCustomers(response.data);
+    } catch (error) {
+      console.error("Failed to load customers", error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   async function handleDelete(id: number) {
@@ -43,35 +50,37 @@ export default function Clients() {
     <div className="p-10 flex flex-col h-full justify-center items-center">
       <h1 className="text-xl">Clientes</h1>
       <div className="w-max p-4 bg-white text-black">
-        {customers.length === 0 ? (
+        {loading ? (
+          <p>Loading...</p>
+        ) : customers.length === 0 ? (
           <p>Nenhum cliente encontrado</p>
         ) : (
           customers.map((customer) => (
-          <div key={customer.id} className="flex border p-10">
-            <div>
-            {customer.name ? <h1>Nome: {customer.name}</h1> : <h1>Nome: Não informado</h1>}
-            {customer.email ? <p>Email: {customer.email}</p> : <p>Email: Não informado</p>}
-            {customer.phone ? <p>Telefone: {customer.phone}</p> : <p>Telefone: Não informado</p>}
-            <hr />
+            <div key={customer.id} className="flex border p-10">
+              <div>
+                {customer.name ? <h1>Nome: {customer.name}</h1> : <h1>Nome: Não informado</h1>}
+                {customer.email ? <p>Email: {customer.email}</p> : <p>Email: Não informado</p>}
+                {customer.phone ? <p>Telefone: {customer.phone}</p> : <p>Telefone: Não informado</p>}
+                <hr />
+              </div>
+              <div>
+                <button
+                  className="h-full font-bold px-2 text-red-500 hover:text-red-700 cursor-pointer ease-linear transition-all"
+                  onClick={() => handleDelete(customer.id)}
+                >
+                  <Trash2 size={24} />
+                </button>
+                <button
+                  className="h-full font-bold px-2 text-blue-500 hover:text-blue-700 cursor-pointer ease-linear transition-all"
+                  onClick={() => router.push(`/clients/${customer.id}`)}
+                >
+                  Visualizar
+                </button>
+              </div>
             </div>
-            <div>
-            <button
-              className="h-full font-bold px-2 text-red-500 hover:text-red-700 cursor-pointer ease-linear transition-all"
-              onClick={() => handleDelete(customer.id)}
-            >
-              <Trash2 size={24}/>
-            </button>
-            <button
-              className="h-full font-bold px-2 text-blue-500 hover:text-blue-700 cursor-pointer ease-linear transition-all"
-              onClick={() => router.push(`/clients/${customer.id}`)}
-            >
-              Visualizar
-            </button>
-            </div>
-          </div>
-            ))
-          )}
-        </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
