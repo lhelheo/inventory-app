3271b30672995490c00e7b1f55281020eeb3ceda
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-10-10 00:56:43 -0300

    fix: routes

--- src/app/(platform)/page.tsx
+++ src/app/(platform)/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import { baseUrl } from '@/helpers/url';
+// import { baseUrl } from '@/helpers/url';
 
 export default function LoginPage() {
     const [username, setUsername] = useState('');
@@ -17,7 +17,7 @@ export default function LoginPage() {
         setLoading(true);
 
         try {
-            const response = await axios.post(`${baseUrl}/login`, { username, password });
+            const response = await axios.post(`https://localhost:3000/login`, { username, password });
 
             if (response.status === 200 && response.data?.token) {
                 console.log('Login successful:', response.data);
