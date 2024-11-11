a499548e557bc6a03076eae90e819927288de176
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-11-11 18:42:02 -0300

    feat: trying to improve payment feature

--- src/app/(platform)/clients/[id]/payment/page.tsx
+++ src/app/(platform)/clients/[id]/payment/page.tsx
@@ -1,70 +1,63 @@
 'use client'
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { baseUrl } from '@/helpers/url'
 
-interface IProduct {
-  id: number
-  name: string
-  price: number
-  remaining_balance: number
-  status: string
-}
+import { baseUrl } from '@/helpers/url'
+import { IProduct } from '@/interface/interfaces'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 interface PaymentPageProps {
-  clientId: string
+  params: {
+    id: string
+  }
 }
 
-export default function PaymentPage({ clientId }: PaymentPageProps) {
+export default function Payment(props: PaymentPageProps) {
   const [products, setProducts] = useState<IProduct[]>([])
-  const [paymentAmount, setPaymentAmount] = useState<{ [key: number]: number }>(
-    {},
-  )
-  const [message, setMessage] = useState<string | null>(null)
-  const [loading, setLoading] = useState<number | null>(null)
+  const [paymentAmount, setPaymentAmount] = useState<number>(0)
+  const [message, setMessage] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+
+  console.log('clientId', props.params.id)
 
   useEffect(() => {
-    const fetchClientProducts = async () => {
+    const fetchClientsProducts = async () => {
       try {
-        const response = await axios.get(`${baseUrl}/client/1/products`)
+        const response = await axios.get(
+          `${baseUrl}/client/${props.params.id}/products`,
+        )
         setProducts(response.data)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
       } catch (error) {
-        setMessage('Erro ao carregar produtos do cliente.')
+        setMessage('Erro ao carregar produtos do cliente')
       }
     }
-    fetchClientProducts()
-  }, [clientId])
+    fetchClientsProducts()
+  }, [props.params.id])
 
-  const handlePayment = async (productId: number) => {
-    setMessage(null)
+  const handlePayment = async () => {
+    setMessage('')
 
-    if (!paymentAmount[productId] || paymentAmount[productId] <= 0) {
-      setMessage('Insira um valor de pagamento válido para o produto.')
-      return
+    if (paymentAmount || paymentAmount <= 0) {
+      setMessage('Valor de pagamento inválido')
     }
 
-    setLoading(productId) // Inicia o loading para o produto específico
+    setLoading(true)
 
     try {
       const response = await axios.post(
-        `${baseUrl}/clients/1/products/${productId}/payment`,
-        { paymentAmount: paymentAmount[productId], productId },
+        `${baseUrl}/client/${props.params.id}/pay`,
+        {
+          paymentAmount,
+        },
       )
-
-      // Atualiza o produto pago na lista de produtos
-      setProducts((prevProducts) =>
-        prevProducts.map((prod) =>
-          prod.id === productId ? response.data.product : prod,
-        ),
-      )
-      setMessage('Pagamento realizado com sucesso!')
-      setPaymentAmount((prev) => ({ ...prev, [productId]: 0 })) // Reseta o valor de pagamento para o produto
+      setProducts(response.data.updatedProducts) // ?
+      setMessage('Pagamento efetuado com sucesso')
+      setPaymentAmount(0)
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      setMessage('Erro ao processar pagamento.')
+      setMessage('Erro ao efetuar pagamento')
     } finally {
-      setLoading(null) // Encerra o loading
+      setLoading(false)
     }
   }
 
@@ -75,60 +68,56 @@ export default function PaymentPage({ clientId }: PaymentPageProps) {
           Pagamento de Produtos
         </h1>
 
-        {products.length > 0 ? (
+        {products ? (
           products.map((product) => (
             <div key={product.id} className="mb-6">
               <p className="text-gray-700 mb-2">
                 <strong>Produto:</strong> {product.name}
               </p>
               <p className="text-gray-700 mb-2">
-                <strong>Preço Total:</strong> R$ {product.price}
+                <strong>Preço Total:</strong> R$ {product.price.toFixed(2)}
               </p>
               <p className="text-gray-700 mb-2">
-                <strong>Valor Pendente:</strong> R$ {product.remaining_balance}
+                <strong>Valor Pendente:</strong> R${' '}
+                {product.pendingAmount?.toFixed(2)}
               </p>
               <p
-                className={`text-gray-700 mb-4 ${product.status === 'Disponível' ? 'text-green-600' : ''}`}
+                className={`text-gray-700 mb-4 ${
+                  product.status === 'Disponivel' ? 'text-green-600' : ''
+                }`}
               >
                 <strong>Status:</strong> {product.status}
               </p>
-
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault()
-                  handlePayment(product.id)
-                }}
-                className="flex flex-col gap-4"
-              >
-                <input
-                  type="number"
-                  step="0.01"
-                  value={paymentAmount[product.id] || ''}
-                  onChange={(e) =>
-                    setPaymentAmount((prev) => ({
-                      ...prev,
-                      [product.id]: parseFloat(e.target.value),
-                    }))
-                  }
-                  className="input"
-                  placeholder="Valor do pagamento"
-                  required
-                />
-                <button
-                  type="submit"
-                  className="py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 transition-all"
-                  disabled={loading === product.id}
-                >
-                  {loading === product.id
-                    ? 'Processando...'
-                    : 'Realizar Pagamento'}
-                </button>
-              </form>
             </div>
           ))
         ) : (
           <p>Carregando produtos...</p>
         )}
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            handlePayment()
+          }}
+          className="flex flex-col gap-4"
+        >
+          <input
+            type="number"
+            step="0.01"
+            value={paymentAmount || ''}
+            onChange={(e) => setPaymentAmount(parseFloat(e.target.value))}
+            className="input"
+            placeholder="Valor do pagamento"
+            required
+          />
+          <button
+            type="submit"
+            className="py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 transition-all"
+            disabled={loading}
+          >
+            {loading ? 'Processando...' : 'Realizar Pagamento'}
+          </button>
+        </form>
 
         {message && (
           <p className="mt-4 text-center text-green-600">{message}</p>
