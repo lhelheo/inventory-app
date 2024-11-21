84efe151ee652e096af1ede75cf7dc00110f21e5
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-11-21 00:23:19 -0300

    feat: token validation login

--- src/app/(platform)/home/page.tsx
+++ src/app/(platform)/home/page.tsx
@@ -1,17 +1,7 @@
 'use client'
 import { HomeForm } from './component'
-import { useEffect } from 'react'
-import router from 'next/router'
 
 export default function Login() {
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-
-    if (!token) {
-      router.push('/')
-    }
-  }, [router])
-
   return (
     <div className="flex flex-col min-h-screen justify-center items-center bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-xl w-full text-center">
