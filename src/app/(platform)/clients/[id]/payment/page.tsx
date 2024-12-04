6d23ce156b2d1c03b8d222c26eaa0e0d09f8b37b
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-12-04 12:05:28 -0300

    style: text color payment page

--- src/app/(platform)/clients/[id]/payment/page.tsx
+++ src/app/(platform)/clients/[id]/payment/page.tsx
@@ -137,14 +137,20 @@ export default function ClientPayment(props: ClientPaymentProps) {
 
         {products.length > 0 && (
           <div className="mt-8">
-            <h2 className="text-xl font-bold mb-4">Status dos Produtos</h2>
+            <h2 className="text-xl font-bold mb-4 text-[#e3e3e3]">
+              Status dos Produtos
+            </h2>
             <table className="min-w-full table-auto border-collapse border border-gray-300">
               <thead>
                 <tr>
-                  <th className="px-4 py-2 border-b text-left">Produto</th>
+                  <th className="px-4 py-2 border-b text-left text-[#e3e3e3]">
+                    Produto
+                  </th>
 
-                  <th className="px-4 py-2 border-b text-left">Preço</th>
-                  <th className="px-4 py-2 border-b text-left">
+                  <th className="px-4 py-2 border-b text-left text-[#e3e3e3]">
+                    Preço
+                  </th>
+                  <th className="px-4 py-2 border-b text-left text-[#e3e3e3]">
                     Saldo Pendente
                   </th>
                 </tr>
