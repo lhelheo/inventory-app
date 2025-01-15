5fd81d8e450fc8f094d00d0bbc90479aede25c6d
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2025-01-15 10:10:54 -0300

    fix: loading circle

--- src/component/loadingCircle.tsx
+++ src/component/loadingCircle.tsx
@@ -1,25 +1,35 @@
 export const LoadingCircle = () => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-[#181818] bg-opacity-90 z-50">
+    <div className="flex justify-center items-center h-screen bg-[#181818] bg-opacity-90">
       <svg
-        className="animate-spin h-8 w-8 text-[#e3e3e3]"
+        className="animate-spin"
+        width="32"
+        height="32"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
       >
+        {/* Círculo de fundo (cinza claro) */}
         <circle
           className="opacity-25"
           cx="12"
           cy="12"
           r="10"
-          stroke="currentColor"
+          stroke="#c4c4c4" /* Cinza claro */
           strokeWidth="4"
         ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C4.477 0 0 4.477 0 12h4z"
-        ></path>
+
+        {/* Barra de carregamento (cinza escuro) */}
+        <circle
+          className="opacity-100"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="#6b6b6b" /* Cinza escuro */
+          strokeWidth="4"
+          strokeDasharray="80"
+          strokeDashoffset="60"
+        ></circle>
       </svg>
     </div>
   )
